f8ab0e62312f64a1cac5d8a6d6c7f64ba7a65e73
Thomas Garcia <tjg9265@yahoo.com>
2025-11-21 08:45:50 -0700

    update 3.9

--- routes/static.js
+++ routes/static.js
@@ -2,16 +2,11 @@ const express = require("express");
 const router = express.Router();
 const baseController = require("../controllers/baseController");
 
-// Serve static files
-router.use(express.static("public"));
-router.use("/css", express.static("public/css"));
-router.use("/js", express.static("public/js"));
-router.use("/images", express.static("public/images"));
-
-// Home route
+// Home page
 router.get("/", baseController.buildHome);
 
 module.exports = router;
+
 
 
 
